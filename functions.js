1bac73ade25fcd27467f2b92f330450ee9257ad3
tLORDt <101082389+tLORDt@users.noreply.github.com>
2023-11-07 15:55:44 +1300

    Name Change of repository

--- functions.js
+++ functions.js
@@ -1,8 +1,8 @@
 function processMessage() {
+    // testing getting the input from user
     let message = document.getElementById("message").value;
+
     console.log(message);
 
-
     return false;
-
-  }
+}
