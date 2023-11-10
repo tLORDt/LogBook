ab3f263bd5b03fe461e3d7ad39e014d2117a2dcf
tLORDt <101082389+tLORDt@users.noreply.github.com>
2023-11-10 14:37:37 +1300

    Website Adjustments

--- functions.js
+++ functions.js
@@ -1,8 +1,10 @@
 function processMessage() {
     // testing getting the input from user
     let message = document.getElementById("message").value;
+    let name = document.getElementById("username").value;
+    console.log("Their name is " + name + ", and they wnat to tell you: \"" + message + "\"");
 
-    console.log(message);
-
+    alert("Message sent!");
+    
     return false;
 }
